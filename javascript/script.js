cfb87dd357bafb6c7640f310e685a6284d4f9eb5
Samer Valente <samervalente@gmail.com>
2022-04-05 00:17:45 -0300

    Versão Finalizada - Todas as Funcionalidades

--- javascript/script.js
+++ javascript/script.js
@@ -1,77 +1,111 @@
+let nomePrato = "";
+let nomeBebida = "";
+let nomeSobremesa = "";
 
-function getCategoria(elementoproduto){
-   let categoria = "";
-   if(elementoproduto.classList.contains("prato")){
-       categoria = "prato";
-   }else if(elementoproduto.classList.contains("bebida")){
-       categoria = "bebida";
-   }else if(elementoproduto.classList.contains("sobremesa")){
-    categoria = "sobremesa";
+let precoPrato; 
+let precoBebida;
+let precoSobremesa;
+
+
+function convertpreco(preco){
+    preco = preco.replace("R$", "").replace(",",".");
+    preco = Number(preco)
+    return preco
 }
 
-return categoria
+function selecionarprato(elemento){
+   const selecionado = document.querySelector(".pratos .selecionado")
+  
+
+   //Se já existir um elemento selecionado (ou seja, um elemento que possua a classe selecionado)
+   if(selecionado != null){
+       selecionado.classList.remove("selecionado")
+   }
+   elemento.classList.add("selecionado")
+   //acessando através do querySelector, as classes nomeproduto e preco existentes dentro do elemento "elementoprato"
+   let preco = elemento.querySelector(".preco").innerHTML
+   nomePrato = elemento.querySelector(".nomeproduto").innerHTML
+   precoPrato = convertpreco(preco);
+   VerificarPedido()
 }
 
-let bebidaselecionada = null;
-let pratoselecionado = null;
-let sobremesaselecionada = null;
-let nomeProduto = "";
-let precoProduto = "";
-let precoTotal;
-
-function selecionarproduto(elementoproduto){
-    const caixaSelecionada = document.querySelector(".selecionado")
-    const categoria = getCategoria(elementoproduto)
-
-    //Selecionando produto-content   
+function selecionarbebida(elemento){
+    const selecionado = document.querySelector(".bebidas .selecionado")
+   
  
-    elementoproduto.classList.toggle("selecionado") 
-    if(categoria == "prato"){
-       pratoselecionado = true;
-       ;
+    //Se já existir um elemento selecionado (ou seja, um elemento que possua a classe selecionado)
+    if(selecionado != null){
+        selecionado.classList.remove("selecionado")
     }
-    else if(categoria == "bebida"){
-        bebidaselecionada = true
-       
-    }else if(categoria == "sobremesa"){
-        sobremesaselecionada = true;
+    elemento.classList.add("selecionado")
+    //acessando através do querySelector, as classes nomeproduto e preco existentes dentro do elemento "elementoprato"
+    let preco = elemento.querySelector(".preco").innerHTML
+    nomeBebida = elemento.querySelector(".nomeproduto").innerHTML
+    precoBebida = convertpreco(preco)
+    VerificarPedido()
+ 
+ }
+
+ function selecionarsobremesa(elemento){
+    const selecionado = document.querySelector(".sobremesas .selecionado")
+  
+ 
+    //Se já existir um elemento selecionado (ou seja, um elemento que possua a classe selecionado)
+    if(selecionado != null){
+        selecionado.classList.remove("selecionado")
     }
-    if(pratoselecionado = true && bebidaselecionada == true && sobremesaselecionada == true){
-        document.querySelector(".selecionar").style.background = "green";
-        document.querySelector(".info-pedidos").querySelector("h4").innerHTML = "Fechar pedido"
-        
+    elemento.classList.add("selecionado")
+    //acessando através do querySelector, as classes nomeproduto e preco existentes dentro do elemento "elementoprato"
+    let preco = elemento.querySelector(".preco").innerHTML
+    nomeSobremesa = elemento.querySelector(".nomeproduto").innerHTML
+    precoSobremesa = convertpreco(preco)
+    VerificarPedido()
+ }
+
+ function VerificarPedido(){
+    console.log(precoPrato, precoBebida, precoSobremesa)
+    if(precoPrato !== undefined && precoBebida !== undefined && precoSobremesa !== undefined){
+        document.querySelector(".selecionar").style.background = "#32B72F"
+        document.querySelector(".selecionar").querySelector("h4").innerHTML = "Fechar Pedido"
     }
 
-  nomeProduto = document.querySelector(".container").querySelector(".nomeproduto").innerHTML
-  precoProduto = document.querySelector(".container").querySelector(".preco").innerHTML
+ }
+ let precototal;
+ function fecharpedido(){
+    precototal = precoPrato + precoBebida+precoSobremesa
+    precototal = precototal.toFixed(2)
 
-  document.querySelector(".comanda").querySelector(".nomeproduto").innerTEXT = nomeProduto
-  document.querySelector(".comanda").querySelector(".preco").innerTEXT = precoProduto;
+    document.querySelector(".container").style.opacity="0.3";
+    document.querySelector("body").classList.add("hiddenoverflow")
+    document.querySelector(".box").classList.remove("hidden")
 
+    document.querySelector(".prato").querySelector(".nomeproduto").innerHTML = nomePrato;
+    document.querySelector(".bebida").querySelector(".nomeproduto").innerHTML = nomeBebida;
+    document.querySelector(".sobremesa").querySelector(".nomeproduto").innerHTML = nomeSobremesa;
+
+    document.querySelector(".prato").querySelector(".preco").innerHTML = precoPrato.toFixed(2)
+    document.querySelector(".bebida").querySelector(".preco").innerHTML = precoBebida.toFixed(2)
+    document.querySelector(".sobremesa").querySelector(".preco").innerHTML = precoSobremesa.toFixed(2)
+    document.querySelector(".precototal").innerHTML = `<strong>R$</strong> ${precototal}` 
+
+ }
+
+ let nome = "";
+ let endereco = "";
+
+function getdata(){
+ nome = prompt("Qual é o seu nome?")
+ endereco = prompt("Qual é o seu endereço?")    
 }
 
-   function fecharpedido(){
-    if(pratoselecionado != true || bebidaselecionada != true || sobremesaselecionada != true){
-        alert("Selecione ao menos 3 itens")
-        document.querySelector(".selecionar").style.background = "#CBCBCB";
-   }
-   document.querySelector(".container").style.opacity = "0.3"
-   const comanda = document.querySelector(".comanda")
-   comanda.classList.remove("hidden")
+function finalizar (){
+    getdata();
+    const msgwhats = `Olá, gostaria de fazer o pedido:\n- Prato: ${nomePrato}\n- Bebida: ${nomeBebida}\n- Sobremesa: ${nomeSobremesa}\nTotal: R$ ${precototal}\n Nome: ${nome}\n Endereço: ${endereco}`;
+    const linkwhats = `https://wa.me/5591988580205?text=${encodeURIComponent(msgwhats)}`;
+    window.open(linkwhats)
+}
 
-   
-
-   
-   }
-
-   function cancelar(){
-       document.querySelector(".comanda").classList.add("hidden")
-       document.querySelector(".container").style.opacity = "1";
-   }
-   
- 
- 
-
-
-
-
+function cancelar(){
+    document.querySelector(".box").classList.add("hidden")
+    document.querySelector(".container").style.opacity="1";
+}
